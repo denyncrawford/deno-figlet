8d4ca1d44c8d3c7cace90c0b129db749829636c2
Denyncrawford <kiquerangelb@gmail.com>
2020-07-12 01:26:45 -0300

    bundler correction

--- dist/fonts/mod.js
+++ dist/fonts/mod.js
@@ -1,295 +1,295 @@
 
-  export * as _lcd from './lcd.js'
+  export * as _lcd from './_lcd.js'
   
-  export * as _cosmic from './cosmic.js'
+  export * as _cosmic from './_cosmic.js'
   
-  export * as _bubble from './bubble.js'
+  export * as _bubble from './_bubble.js'
   
-  export * as _smscript from './smscript.js'
+  export * as _smscript from './_smscript.js'
   
-  export * as _smshadow from './smshadow.js'
+  export * as _smshadow from './_smshadow.js'
   
-  export * as _rowancap from './rowancap.js'
+  export * as _rowancap from './_rowancap.js'
   
-  export * as _colossal from './colossal.js'
+  export * as _colossal from './_colossal.js'
   
-  export * as _mike from './mike.js'
+  export * as _mike from './_mike.js'
   
-  export * as _5lineoblique from './5lineoblique.js'
+  export * as _5lineoblique from './_5lineoblique.js'
   
-  export * as _stellar from './stellar.js'
+  export * as _stellar from './_stellar.js'
   
-  export * as _ntgreek from './ntgreek.js'
+  export * as _ntgreek from './_ntgreek.js'
   
-  export * as _eftiwall from './eftiwall.js'
+  export * as _eftiwall from './_eftiwall.js'
   
-  export * as _lockergnome from './lockergnome.js'
+  export * as _lockergnome from './_lockergnome.js'
   
-  export * as _slscript from './slscript.js'
+  export * as _slscript from './_slscript.js'
   
-  export * as _slant from './slant.js'
+  export * as _slant from './_slant.js'
   
-  export * as _eftichess from './eftichess.js'
+  export * as _eftichess from './_eftichess.js'
   
-  export * as _goofy from './goofy.js'
+  export * as _goofy from './_goofy.js'
   
-  export * as _isometric2 from './isometric2.js'
+  export * as _isometric2 from './_isometric2.js'
   
-  export * as _bulbhead from './bulbhead.js'
+  export * as _bulbhead from './_bulbhead.js'
   
-  export * as _isometric4 from './isometric4.js'
+  export * as _isometric4 from './_isometric4.js'
   
-  export * as _jerusalem from './jerusalem.js'
+  export * as _jerusalem from './_jerusalem.js'
   
-  export * as _script from './script.js'
+  export * as _script from './_script.js'
   
-  export * as _hollywood from './hollywood.js'
+  export * as _hollywood from './_hollywood.js'
   
-  export * as _thin from './thin.js'
+  export * as _thin from './_thin.js'
   
-  export * as _shadow from './shadow.js'
+  export * as _shadow from './_shadow.js'
   
-  export * as _rev from './rev.js'
+  export * as _rev from './_rev.js'
   
-  export * as _barbwire from './barbwire.js'
+  export * as _barbwire from './_barbwire.js'
   
-  export * as _stampatello from './stampatello.js'
+  export * as _stampatello from './_stampatello.js'
   
-  export * as _ivrit from './ivrit.js'
+  export * as _ivrit from './_ivrit.js'
   
-  export * as _fuzzy from './fuzzy.js'
+  export * as _fuzzy from './_fuzzy.js'
   
-  export * as _doh from './doh.js'
+  export * as _doh from './_doh.js'
   
-  export * as _drpepper from './drpepper.js'
+  export * as _drpepper from './_drpepper.js'
   
-  export * as _calgphy2 from './calgphy2.js'
+  export * as _calgphy2 from './_calgphy2.js'
   
-  export * as _graffiti from './graffiti.js'
+  export * as _graffiti from './_graffiti.js'
   
-  export * as _avatar from './avatar.js'
+  export * as _avatar from './_avatar.js'
   
-  export * as _marquee from './marquee.js'
+  export * as _marquee from './_marquee.js'
   
-  export * as _relief2 from './relief2.js'
+  export * as _relief2 from './_relief2.js'
   
-  export * as _tanja from './tanja.js'
+  export * as _tanja from './_tanja.js'
   
-  export * as _binary from './binary.js'
+  export * as _binary from './_binary.js'
   
-  export * as _fourtops from './fourtops.js'
+  export * as _fourtops from './_fourtops.js'
   
-  export * as _cybermedium from './cybermedium.js'
+  export * as _cybermedium from './_cybermedium.js'
   
-  export * as _slide from './slide.js'
+  export * as _slide from './_slide.js'
   
-  export * as _mirror from './mirror.js'
+  export * as _mirror from './_mirror.js'
   
-  export * as _straight from './straight.js'
+  export * as _straight from './_straight.js'
   
-  export * as _3_d from './3-d.js'
+  export * as _3_d from './_3_d.js'
   
-  export * as _lean from './lean.js'
+  export * as _lean from './_lean.js'
   
-  export * as _smkeyboard from './smkeyboard.js'
+  export * as _smkeyboard from './_smkeyboard.js'
   
-  export * as _contrast from './contrast.js'
+  export * as _contrast from './_contrast.js'
   
-  export * as _madrid from './madrid.js'
+  export * as _madrid from './_madrid.js'
   
-  export * as _computer from './computer.js'
+  export * as _computer from './_computer.js'
   
-  export * as _standard from './standard.js'
+  export * as _standard from './_standard.js'
   
-  export * as _usaflag from './usaflag.js'
+  export * as _usaflag from './_usaflag.js'
   
-  export * as _ticks from './ticks.js'
+  export * as _ticks from './_ticks.js'
   
-  export * as _catwalk from './catwalk.js'
+  export * as _catwalk from './_catwalk.js'
   
-  export * as _alphabet from './alphabet.js'
+  export * as _alphabet from './_alphabet.js'
   
-  export * as _fender from './fender.js'
+  export * as _fender from './_fender.js'
   
-  export * as _smtengwar from './smtengwar.js'
+  export * as _smtengwar from './_smtengwar.js'
   
-  export * as _alligator2 from './alligator2.js'
+  export * as _alligator2 from './_alligator2.js'
   
-  export * as _contessa from './contessa.js'
+  export * as _contessa from './_contessa.js'
   
-  export * as _weird from './weird.js'
+  export * as _weird from './_weird.js'
   
-  export * as _poison from './poison.js'
+  export * as _poison from './_poison.js'
   
-  export * as _threepoint from './threepoint.js'
+  export * as _threepoint from './_threepoint.js'
   
-  export * as _twopoint from './twopoint.js'
+  export * as _twopoint from './_twopoint.js'
   
-  export * as _moscow from './moscow.js'
+  export * as _moscow from './_moscow.js'
   
-  export * as _rozzo from './rozzo.js'
+  export * as _rozzo from './_rozzo.js'
   
-  export * as _roman from './roman.js'
+  export * as _roman from './_roman.js'
   
-  export * as _peaks from './peaks.js'
+  export * as _peaks from './_peaks.js'
   
-  export * as _gothic from './gothic.js'
+  export * as _gothic from './_gothic.js'
   
-  export * as _sblood from './sblood.js'
+  export * as _sblood from './_sblood.js'
   
-  export * as _serifcap from './serifcap.js'
+  export * as _serifcap from './_serifcap.js'
   
-  export * as _bigchief from './bigchief.js'
+  export * as _bigchief from './_bigchief.js'
   
-  export * as _isometric3 from './isometric3.js'
+  export * as _isometric3 from './_isometric3.js'
   
-  export * as _tengwar from './tengwar.js'
+  export * as _tengwar from './_tengwar.js'
   
-  export * as _o8 from './o8.js'
+  export * as _o8 from './_o8.js'
   
-  export * as _isometric1 from './isometric1.js'
+  export * as _isometric1 from './_isometric1.js'
   
-  export * as _cybersmall from './cybersmall.js'
+  export * as _cybersmall from './_cybersmall.js'
   
-  export * as _diamond from './diamond.js'
+  export * as _diamond from './_diamond.js'
   
-  export * as _eftipiti from './eftipiti.js'
+  export * as _eftipiti from './_eftipiti.js'
   
-  export * as _caligraphy from './caligraphy.js'
+  export * as _caligraphy from './_caligraphy.js'
   
-  export * as _digital from './digital.js'
+  export * as _digital from './_digital.js'
   
-  export * as _nancyj_underlined from './nancyj-underlined.js'
+  export * as _nancyj_underlined from './_nancyj_underlined.js'
   
-  export * as _cricket from './cricket.js'
+  export * as _cricket from './_cricket.js'
   
-  export * as _bell from './bell.js'
+  export * as _bell from './_bell.js'
   
-  export * as _ogre from './ogre.js'
+  export * as _ogre from './_ogre.js'
   
-  export * as _linux from './linux.js'
+  export * as _linux from './_linux.js'
   
-  export * as _ticksslant from './ticksslant.js'
+  export * as _ticksslant from './_ticksslant.js'
   
-  export * as _eftifont from './eftifont.js'
+  export * as _eftifont from './_eftifont.js'
   
-  export * as _pyramid from './pyramid.js'
+  export * as _pyramid from './_pyramid.js'
   
-  export * as _relief from './relief.js'
+  export * as _relief from './_relief.js'
   
-  export * as _term from './term.js'
+  export * as _term from './_term.js'
   
-  export * as _pepper from './pepper.js'
+  export * as _pepper from './_pepper.js'
   
-  export * as _dotmatrix from './dotmatrix.js'
+  export * as _dotmatrix from './_dotmatrix.js'
   
-  export * as _rot13 from './rot13.js'
+  export * as _rot13 from './_rot13.js'
   
-  export * as _smslant from './smslant.js'
+  export * as _smslant from './_smslant.js'
   
-  export * as _starwars from './starwars.js'
+  export * as _starwars from './_starwars.js'
   
-  export * as _nancyj from './nancyj.js'
+  export * as _nancyj from './_nancyj.js'
   
-  export * as _pawp from './pawp.js'
+  export * as _pawp from './_pawp.js'
   
-  export * as _katakana from './katakana.js'
+  export * as _katakana from './_katakana.js'
   
-  export * as _rounded from './rounded.js'
+  export * as _rounded from './_rounded.js'
   
-  export * as _tombstone from './tombstone.js'
+  export * as _tombstone from './_tombstone.js'
   
-  export * as _larry3d from './larry3d.js'
+  export * as _larry3d from './_larry3d.js'
   
-  export * as _short from './short.js'
+  export * as _short from './_short.js'
   
-  export * as _nipples from './nipples.js'
+  export * as _nipples from './_nipples.js'
   
-  export * as _banner4 from './banner4.js'
+  export * as _banner4 from './_banner4.js'
   
-  export * as _trek from './trek.js'
+  export * as _trek from './_trek.js'
   
-  export * as _eftitalic from './eftitalic.js'
+  export * as _eftitalic from './_eftitalic.js'
   
-  export * as _big from './big.js'
+  export * as _big from './_big.js'
   
-  export * as _chunky from './chunky.js'
+  export * as _chunky from './_chunky.js'
   
-  export * as _block from './block.js'
+  export * as _block from './_block.js'
   
-  export * as _univers from './univers.js'
+  export * as _univers from './_univers.js'
   
-  export * as _coinstak from './coinstak.js'
+  export * as _coinstak from './_coinstak.js'
   
-  export * as _runic from './runic.js'
+  export * as _runic from './_runic.js'
   
-  export * as _epic from './epic.js'
+  export * as _epic from './_epic.js'
   
-  export * as _cyberlarge from './cyberlarge.js'
+  export * as _cyberlarge from './_cyberlarge.js'
   
-  export * as _tsalagi from './tsalagi.js'
+  export * as _tsalagi from './_tsalagi.js'
   
-  export * as _italic from './italic.js'
+  export * as _italic from './_italic.js'
   
-  export * as _pebbles from './pebbles.js'
+  export * as _pebbles from './_pebbles.js'
   
-  export * as _speed from './speed.js'
+  export * as _speed from './_speed.js'
   
-  export * as _puffy from './puffy.js'
+  export * as _puffy from './_puffy.js'
   
-  export * as _jazmine from './jazmine.js'
+  export * as _jazmine from './_jazmine.js'
   
-  export * as _cosmike from './cosmike.js'
+  export * as _cosmike from './_cosmike.js'
   
-  export * as _doom from './doom.js'
+  export * as _doom from './_doom.js'
   
-  export * as _banner3_D from './banner3-D.js'
+  export * as _banner3_D from './_banner3_D.js'
   
-  export * as _maxfour from './maxfour.js'
+  export * as _maxfour from './_maxfour.js'
   
-  export * as _letters from './letters.js'
+  export * as _letters from './_letters.js'
   
-  export * as _mini from './mini.js'
+  export * as _mini from './_mini.js'
   
-  export * as _smisome1 from './smisome1.js'
+  export * as _smisome1 from './_smisome1.js'
   
-  export * as _mnemonic from './mnemonic.js'
+  export * as _mnemonic from './_mnemonic.js'
   
-  export * as _basic from './basic.js'
+  export * as _basic from './_basic.js'
   
-  export * as _3x5 from './3x5.js'
+  export * as _3x5 from './_3x5.js'
   
-  export * as _tinker_toy from './tinker-toy.js'
+  export * as _tinker_toy from './_tinker_toy.js'
   
-  export * as _banner from './banner.js'
+  export * as _banner from './_banner.js'
   
-  export * as _banner3 from './banner3.js'
+  export * as _banner3 from './_banner3.js'
   
-  export * as _cursive from './cursive.js'
+  export * as _cursive from './_cursive.js'
   
-  export * as _invita from './invita.js'
+  export * as _invita from './_invita.js'
   
-  export * as _kban from './kban.js'
+  export * as _kban from './_kban.js'
   
-  export * as _wavy from './wavy.js'
+  export * as _wavy from './_wavy.js'
   
-  export * as _eftiwater from './eftiwater.js'
+  export * as _eftiwater from './_eftiwater.js'
   
-  export * as _morse from './morse.js'
+  export * as _morse from './_morse.js'
   
-  export * as _stop from './stop.js'
+  export * as _stop from './_stop.js'
   
-  export * as _alligator from './alligator.js'
+  export * as _alligator from './_alligator.js'
   
-  export * as _small from './small.js'
+  export * as _small from './_small.js'
   
-  export * as _runyc from './runyc.js'
+  export * as _runyc from './_runyc.js'
   
-  export * as _eftirobot from './eftirobot.js'
+  export * as _eftirobot from './_eftirobot.js'
   
-  export * as _nancyj_fancy from './nancyj-fancy.js'
+  export * as _nancyj_fancy from './_nancyj_fancy.js'
   
-  export * as _thick from './thick.js'
+  export * as _thick from './_thick.js'
   
-  export * as _acrobatic from './acrobatic.js'
+  export * as _acrobatic from './_acrobatic.js'
   
